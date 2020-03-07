90e22b4d4edc7c5b62dfb736dca97829e928bb9d
Austin <austin.hilberg@gmail.com>
2020-03-06 21:17:42 -0600

    Add GameManager
    Fixes #2.
    
    Completes Barebones milestone.

---
+++ GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    HexBoard board;
+    Player[] players;
+    enum GameState
+    {
+        Decision,
+        Resolution,
+        Completion
+    }
+    GameState state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
